de3471e08bb8691b8a00c4aaaf8174808c522c33
Areeba Naeem <areebanaeem069@gmail.com>
2025-01-23 14:43:18 +0500

    Update script.js

--- Project2- KeyBoard Magics/script.js
+++ Project2- KeyBoard Magics/script.js
@@ -1,1 +1,24 @@
 
+
+// method 1 
+
+const insert=document.getElementById('insert');
+window.addEventListener('keydown',(e)=>{// write  window bcz we want  to call this in whole window
+    insert.insertAdjacentHTML('beforeend', `
+        <div class='color'>
+            <table>
+                <tr>
+                    <th>Key</th>
+                    <th>Key Code</th>
+                    <th>Code</th>
+                </tr>
+                <tr>
+                    <td>${e.key === " " ? "Space" : e.key}</td>
+                    <td>${e.keyCode}</td>
+                    <td>${e.code}</td>
+                </tr>
+            </table>
+        </div>
+    `);
+})
+
